fbf2a63902e29c88a5b325caa4eafecb5b1853a3
hecoche <121912192+hecoche@users.noreply.github.com>
2023-01-04 09:49:45 +0800

    Add files via upload

---
+++ static/lang/hanY.js
@@ -0,0 +1,46 @@
+module.exports = {
+    message: {
+        whitePaper: '백서',
+        baog:"감사보고서",
+        home:"첫 페이지",
+        ecological: '종목 소개',
+        projectIntroductionCon: '상아생태"의 목표는 높은 인기와 높은 가치를 가진 스타 화폐 che를 만드는 것입니다. 10년 무손실 채광 메커니즘을 만들 뿐만 아니라 현재 유행하는 다사선 융합방식, nft 등 방식을 융합하여 하나의 새로운 신화와 전설을 구축하고 공동으로 부의 미래를 창조합니다!스마트 컨트랙은 완전한 탈중심화, 오픈 소스 코드, 커뮤니티 자치, 프리 파킹, 프리 오픈 프라이스, 프리 오픈 프라이스, 프리 팀, 토큰은 모두 블랙홀 파괴, 채굴 풀 저당, 추가 유동성과 모두의 공평한 참여.',
+        save:"입금",
+        zyInput: "파",
+        time:"채굴 기한",
+        theTotal: "이번 라운드 합계",
+        overallPledge: "누계 총질권",
+        check:"CHE 계약 보기",
+        
+
+        liu:"총유통량",
+        banner:"잔고",
+        allEarn:"누적 수익",
+        unExtract:"미인출수익",
+        tq:"che를 뽑다",
+        authorization: "수여 권",
+        zy: "질 걸",
+        myZy:"나의 질물",
+        exitPledge: "질권으로부터 탈퇴하다",
+
+
+        buy:"체 트레이딩",
+        dealSuccee:"거래 성공!",
+        noBanner:"잔고가 부족하다!",
+        gas:"가스 부족!",
+        pack:"포장중...",
+        accredit:"인증에 성공했습니다!",
+        noAccredit:"권한이 없습니다!",
+        isUse:"커뮤니티 번호를 사용하셨습니다, 하나의 순수한 디지털 커뮤니티 번호를 바꾸십시오!",
+        okCircle:"성공적으로 만들었습니다",
+        okJoin:"가입 성공",
+        noMask:"블록 체인 dapp 브라우저를 이용해서 열어 주세요.",
+        noNum:"액수는 0보다 작으면 안 된다",
+        change:"거래",
+        liquidity:"유동성 추가",
+        double:"수익",
+        fee:"수수료가 부족하다!",
+        LOCKEDusdt:'창고 잠금 수량',
+        years:'년'
+    },
+};
